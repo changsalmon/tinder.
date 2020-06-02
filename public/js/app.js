26cb5eee408ed78b16877a1ce0838e13421be717
arata-okada <araoka.psn0121@gmail.com>
2020-06-02 17:27:18 +0900

    [add] refs #2 プロフィール画面作成
    プロフィール画面でのユーザー情報表示とストレージリンク

--- public/js/app.js
+++ public/js/app.js
@@ -37320,8 +37320,8 @@ $(document).on("change", "#file_photo", function (e) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\ArataOkada\Desktop\App\techpit-match\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\ArataOkada\Desktop\App\techpit-match\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\ArataOkada\Desktop\App\tinder\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\ArataOkada\Desktop\App\tinder\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
