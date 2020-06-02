9e54a6a60c4540ec4a2782f50320d6d7187f5e20
arata-okada <araoka.psn0121@gmail.com>
2020-06-02 16:14:31 +0900

    [update] サービスファイルへの切り分け
    RegisterControllerのcreateのファットコントローラー防止

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Intervention\Image\Facades\Image;
+use App\Services\CheckExtensionServices;
+use App\Services\FileUploadServices;
 
 class RegisterController extends Controller
 {
@@ -69,32 +71,12 @@ class RegisterController extends Controller
     {
         $imageFile = $data['img_name'];
 
-        $filenameWithExt = $imageFile->getClientOriginalName();
-        
-        $fileName = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-        
-        $extension = $imageFile->getClientOriginalExtension();
-        
-        $fileNameToStore = $fileName.'_'.time().'.'.$extension;
-        
-        $fileData = file_get_contents($imageFile->getRealPath());
-        
-        if ($extension = 'jpg') {
-            $data_url = 'data:image/jpg;base64,'. base64_encode($fileData);
-        }
-        
-        if ($extension = 'jpeg') {
-            $data_url = 'data:image/jpg;base64,'. base64_encode($fileData);
-        }
-        
-        if ($extension = 'png') {
-            $data_url = 'data:image/png;base64,'. base64_encode($fileData);
-        }
-        
-        if ($extension = 'gif') {
-            $data_url = 'data:image/gif;base64,'. base64_encode($fileData);
-        }
-        
+        $list = FileUploadServices::fileUpload($imageFile);
+
+        list($extension, $fileNameToStore, $fileData) = $list;
+
+        $data_url = CheckExtensionServices::checkExtension($fileData, $extension);
+           
         $image = Image::make($data_url);
         
         $image->resize(400, 400)->save(storage_path() . '/app/public/images/' . $fileNameToStore);
