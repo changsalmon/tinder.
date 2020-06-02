80e8a13608eaea3df7db1ca173f33a2f5607ed4a
arata-okada <araoka.psn0121@gmail.com>
2020-06-02 18:01:13 +0900

    [add] refs #3 プロフィールの編集機能追加
    ルーティング、コントローラーなど

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -21,8 +21,7 @@
     
       <div class='userAction'>
         <div class="userAction_edit userAction_common">
-          <i class="fas fa-edit fa-2x"></i>
-          <span>情報を編集</span>
+        <a href="/users/edit/{{$user->id}}"><i class="fas fa-edit fa-2x"></i></a>          <span>情報を編集</span>
         </div>
         <div class='userAction_logout userAction_common'>
         <a href="{{ route('logout') }}" onclick="event.preventDefault();
